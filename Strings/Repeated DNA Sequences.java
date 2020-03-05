981bfd830855e86f9368ecfd4cb33a7fcb5825c3
Kishore Kumar Pechetti <kpechetti@leomail.tamuc.edu>
2020-03-05 08:38:44 -0800

    Create Repeated DNA Sequences.java

---
+++ Strings/Repeated DNA Sequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> seen = new HashMap<>();      
+        
+        int i=0;        
+        while(i+10 <= s.length())
+        {
+            String str = s.substring(i, i++ + 10);
+            seen.put(str, seen.getOrDefault(str,0)+1);
+            
+            if(seen.get(str) == 2)
+            {
+                result.add(str);
+            }
+        }
+        
+        return result;
+    }
+}
